eb21e9509b8e381167c1df022fb730c2927392ea
Martín <magadiflo@gmail.com>
2022-01-03 18:41:54 -0500

    Creando la clase entity de persistencia

--- src/main/java/org/magadiflo/hibernate/app/entity/Cliente.java
+++ src/main/java/org/magadiflo/hibernate/app/entity/Cliente.java
@@ -1,4 +1,72 @@
 package org.magadiflo.hibernate.app.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "clientes")
 public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+
+    private String apellido;
+
+    @Column(name = "forma_pago")
+    private String formaPago;
+
+    public Cliente() {
+    }
+
+    public Cliente(Long id, String nombre, String apellido, String formaPago) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.formaPago = formaPago;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getFormaPago() {
+        return formaPago;
+    }
+
+    public void setFormaPago(String formaPago) {
+        this.formaPago = formaPago;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Cliente{");
+        sb.append("id=").append(id);
+        sb.append(", nombre='").append(nombre).append('\'');
+        sb.append(", apellido='").append(apellido).append('\'');
+        sb.append(", formaPago='").append(formaPago).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
 }
