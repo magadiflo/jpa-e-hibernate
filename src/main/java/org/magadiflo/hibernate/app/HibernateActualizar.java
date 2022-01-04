21ec6257f139724c6f1e280d0eb603571002d3de
Martín <magadiflo@gmail.com>
2022-01-04 17:07:55 -0500

    Actualizar registros con el método merge del EntityManager

---
+++ src/main/java/org/magadiflo/hibernate/app/HibernateActualizar.java
@@ -0,0 +1,28 @@
+package org.magadiflo.hibernate.app;
+
+import jakarta.persistence.EntityManager;
+import org.magadiflo.hibernate.app.entity.Cliente;
+import org.magadiflo.hibernate.app.util.JpaUtil;
+
+public class HibernateActualizar {
+    public static void main(String[] args) {
+        EntityManager em = JpaUtil.getEntityManager();
+        try {
+            em.getTransaction().begin();
+
+            Cliente cliente = new Cliente();
+            cliente.setId(1L);
+            cliente.setNombre("Martín Gaspar");
+            cliente.setApellido("Díaz Flores");
+            cliente.setFormaPago("credito");
+
+            em.merge(cliente);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+            e.printStackTrace();
+        } finally {
+            em.close();
+        }
+    }
+}
